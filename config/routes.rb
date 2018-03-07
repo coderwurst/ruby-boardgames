85a65d7dcc2ec30f27dedf620b254ed584f42159
Andrew McMechan <coderwurst@users.noreply.github.com>
2018-03-07 21:19:15 +0100

    add root route for homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'boardgames#index'
+  resources :boardgames
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
